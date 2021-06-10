538fa77f1b6cd9119e15aa3c210d99c03502c18a
fancydevpro <41801970+fancydevpro@users.noreply.github.com>
2021-06-10 21:11:54 +0200

    created api request service

---
+++ src/services/api-request.service.ts
@@ -0,0 +1,22 @@
+import axios, { AxiosInstance, Method, AxiosResponse } from 'axios';
+
+const apiInstance: AxiosInstance = axios.create({
+  baseURL: 'https://api.github.com',
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+});
+
+export function setApiTimeout(timeout: number): void {
+  apiInstance.defaults.timeout = timeout;
+}
+
+export function ApiRequest<T>(
+  url: string,
+  method: Method = 'get',
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  { params, data, headers }: { params?: any; data?: any; headers?: any } = {},
+): Promise<AxiosResponse<T>> {
+  return apiInstance.request({ url, method, params, data, headers });
+}
